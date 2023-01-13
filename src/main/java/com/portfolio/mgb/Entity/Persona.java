c25ba0b0e4a11531006181e395c1f5783260791b
Juan Martinez <ing.juan.martinez.88@gmail.com>
2023-01-13 10:12:01 -0300

    agregando el backEnd

---
+++ src/main/java/com/portfolio/mgb/Entity/Persona.java
@@ -0,0 +1,43 @@
+package com.portfolio.mgb.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter @Setter
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String nombre;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String apellido;
+    
+    @NotNull
+    private String descripcion;
+    
+    private String img;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String descripcion, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.img = img;
+    }       
+    
+}
